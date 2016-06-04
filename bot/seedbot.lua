dfb65bd72d39840f2adb4c74490d6f78762d010d
Android092921 <farzadnari954@gmail.com>
2016-06-05 08:57:46 +0900

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -216,7 +216,6 @@ function create_config( )
     "stats",
     "anti_spam",
     "owners",
-    "arabic_lock",
     "set",
     "get",
     "broadcast",
@@ -226,30 +225,18 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {182420116},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[PARADOX v1
+An advance Administration bot based on Teleseed
 
-https://github.com/SEEDTEAM/TeleSeed
 
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Developer:
+Farzad Nari( @Farzad_Nari )
 
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+Support Channel:
+@Paradox_Ch
 ]],
     help_text_realm = [[
 Realm Commands:
